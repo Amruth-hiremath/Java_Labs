0bf57aaa7c2e62f53064c2044f264acb57b356b8
RVU <RVU@10.17.5.24>
2026-01-23 10:58:59 +0530

    Latest batch of files lol

---
+++ Amruthesh_1RUA24CSE0042/src/lab1_f.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class lab1_f {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//input 3 integers and display the largest and smallest
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter second number: ");
+		int num2 = sc.nextInt();
+		System.out.println("Enter third number: ");
+		int num3 = sc.nextInt();
+		
+		if(num1 > num2 && num1 > num3) {
+			System.out.println("Greatest number is " + num1);
+		}
+		else if(num2 > num1 && num2 > num3) {
+			System.out.println("Greatest number is " + num2);
+		}
+		else {
+			System.out.println("Greatest number is " + num3);
+		}
+		
+		if(num1 < num2 && num1 < num3) {
+			System.out.println("Smallest number is " + num1);
+		}
+		else if(num2 < num1 && num2 < num3) {
+			System.out.println("Smallest number is " + num2);
+		}
+		else {
+			System.out.println("Smallest number is " + num3);
+		}
+		
+		sc.close();
+	}
+
+}
