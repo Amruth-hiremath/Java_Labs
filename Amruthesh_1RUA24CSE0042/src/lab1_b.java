be900551e9190a34a01ef18078212dc215100403
RVU <RVU@10.17.5.24>
2026-01-23 10:26:48 +0530

    Initial commit of java files

---
+++ Amruthesh_1RUA24CSE0042/src/lab1_b.java
@@ -0,0 +1,15 @@
+
+public class lab1_b {
+
+	public static void main(String[] args) {
+		String name = "Amruthesh";
+		int age = 18;
+		String branch = "CSE";
+		
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Branch: " + branch);
+
+	}
+
+}
---
+++ Amruthesh_1RUA24CSE0042/src/lab1_d.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class lab1_d {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = sc.nextLine();
+		System.out.println("Welcome " + name);
+	}
+
+}
