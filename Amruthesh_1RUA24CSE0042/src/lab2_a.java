2ec758cd8c4dbb8621e5717fdbde03657d86da80
RVU <RVU@10.17.18.36>
2026-01-30 09:43:21 +0530

    Lab2_a: WAP to print 1-10 using for loop and while loop.

---
+++ Amruthesh_1RUA24CSE0042/src/lab2_a.java
@@ -0,0 +1,18 @@
+
+public class lab2_a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Using for loop: ");
+		for(int i = 1; i < 11; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Using while loop: ");
+		int n = 1;
+		while(n <= 10) {
+			System.out.println(n);
+			n++;
+		}
+	}
+
+}
