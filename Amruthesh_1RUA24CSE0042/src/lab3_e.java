55ff9cde5cc6286f547d297dc5f1b557e4943afe
RVU <RVU@10.17.3.22>
2026-02-06 10:56:06 +0530

    Lab3 files - Lab Manual

--- Amruthesh_1RUA24CSE0042/src/lab3_e.java
+++ Amruthesh_1RUA24CSE0042/src/lab3_e.java
@@ -1,9 +1,12 @@
-
+import java.util.Scanner;
 public class lab3_e {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i < 5; i++) {
+		System.out.println("Enter the number of rows: ");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i = 0; i <= num; i++) {
 			for(int j = 0; j < i; j++) {
 				System.out.print(i);
 			}
--- Amruthesh_1RUA24CSE0042/src/lab3_g.java
+++ Amruthesh_1RUA24CSE0042/src/lab3_g.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 
 public class lab3_g {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a[] = {1,2,3,4,5};
-		for(int i = 4; i >= 0; i--) {
-			System.out.println(a[i]);
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter array size: ");
+
+		int n= sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the elements: ");
+
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
 		}
-
+		System.out.println("Reversed array: ");
+		for(int i=n-1;i>=0;i--) {
+			System.out.print(arr[i] + " ");
+		}
 	}
-
 }
