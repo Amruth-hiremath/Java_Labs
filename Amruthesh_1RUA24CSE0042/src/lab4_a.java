a63ec7b0978145b7e39bed65c77d23d5ae99f635
RVU <RVU@10.17.24.225>
2026-02-13 11:03:54 +0530

    Lab Program 3 files

---
+++ Amruthesh_1RUA24CSE0042/src/lab4_a.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Circle{
+	int radius;
+	
+	double circumference(int radius) {
+		double circumference = radius * 3.14 * 2;
+		return circumference;
+	}
+}
+public class lab4_a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub3
+		Circle c = new Circle();
+		System.out.println("Enter the radius: ");
+		Scanner sc = new Scanner(System.in);
+		int rad = sc.nextInt();
+		double result = c.circumference(rad);
+		System.out.println("The circumference of the circle is: " + result);
+		sc.close();
+
+	}
+
+}
