1c41744899852d8a83eb4ce996bd2cc3b52f4faf
Amruth-hiremath <amrutheshchiremathbtech24@rvu.edu.in>
2026-03-12 00:09:47 +0530

    Part B Lab 1

---
+++ Amruthesh_1RUA24CSE0042/src/lab6_part_b/lab1.java
@@ -0,0 +1,26 @@
+package lab6_part_b;
+
+import java.util.Scanner;
+
+public class lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number of rows in the triangle: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
